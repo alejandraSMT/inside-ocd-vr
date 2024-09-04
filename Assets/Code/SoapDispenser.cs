3c841fe6de437b6c8e25e0eea3c6562a2ffea6b4
frankvic03 <frankviva0307@gmail.com>
2024-09-04 16:26:24 -0500

    update soap

---
+++ Assets/Code/SoapDispenser.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SoapDispenser : MonoBehaviour
+{
+
+    public GameObject currentBubbles;
+    
+    // Prefab de burbujas
+    //crear un bool  si comenzo
+    bool comenzo = false;
+    float timmer = 2f;
+    //llamar update
+    public void Awake(){
+        //currentBubbles.GetComponent<ParticleSystem>().Pause();
+        currentBubbles.SetActive(false);
+    }
+
+    public void Update(){
+        if(comenzo){
+            if(timmer > 0){
+                timmer -= Time.deltaTime;
+            }else{
+                timmer = 2f;
+                currentBubbles.GetComponent<ParticleSystem>().Clear();
+                currentBubbles.GetComponent<ParticleSystem>().Pause();
+                comenzo = false;
+            }
+        }
+    }
+    // Asigna esta función a una acción de Input
+    public void StartBubble()
+    {
+        if(!comenzo){
+            //intelicence autocompletat unity
+            // Instanciar las burbujas en la posición del dispensador
+
+            GameObject aux =  currentBubbles;
+            currentBubbles.SetActive(true);
+            aux.GetComponent<ParticleSystem>().Play();
+            Debug.Log("se puso Play");    
+            //StartCoroutine(Wait());
+            //aux.GetComponent<ParticleSystem>().Pause();  
+            //Debug.Log("se puso Pause");   
+            comenzo=true;
+        }
+       
+            
+    }
+
+    private IEnumerator Wait(){
+        yield return new WaitForSecondsRealtime(1f);
+    }
+}
